73fc67b29138fd007752510cf268fd7ca8285ec9
Rhys Walden <rhys.walden.dev@gmail.com>
2017-07-13 21:00:34 +1200

    start of dev

---
+++ DNA.RestServices/DNA.RestServices.Core/Molecule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNA.RestServices.Core
+{
+    public class MoleculeType
+    {
+        public const string ResourceMolecule = "ResourceMolecule";
+    }
+
+
+    public class Molecule
+    {
+        public Molecule(string type)
+        {
+            this.Type = type;
+        }
+
+        public string Type { get;  private set; }
+    }
+
+    public class UIMolecule : Molecule
+    {
+
+    }
+
+    public class TextMolecule : UIMolecule
+    {
+
+    }
+
+    public class InputMolecule : UIMolecule
+    {
+
+    }
+
+    public class ImageMolecule : UIMolecule
+    {
+
+    }
+
+    public class ResourceMolecule : Molecule
+    {
+        public ResourceMolecule():base(MoleculeType.ResourceMolecule)
+        {
+
+        }
+
+        public string Key { get; set; }
+        public long UTCTimestamp { get; set; } 
+        public string Culture { get; set; }
+    }
+}
