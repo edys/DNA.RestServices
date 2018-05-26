d783ebb4b121d5dbd5fa4d2e55b1b2010dbe58d5
Rhys Walden <rhys.walden.dev@gmail.com>
2018-05-26 15:06:34 +1200

    update

---
+++ DNA.BusinessLogic/Hello.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DNA.BusinessLogic
+{
+    public class Hello
+    {
+        public Hello()
+        {
+            Console.WriteLine("=====Hello=====");
+        }
+
+        public void SaySomething()
+        {
+            Console.WriteLine("=====It's Me=====");
+        }
+    }
+}
